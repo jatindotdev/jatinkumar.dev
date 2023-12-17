76e71c40226525fe2955ebbdec4694ba878cc11a
Jatin Kumar <main.jatink@gmail.com>
2023-12-18 03:48:37 +0530

    chore: format files

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -4,13 +4,13 @@
  * For more information, see https://remix.run/file-conventions/entry.server
  */
 
-import { PassThrough } from "node:stream";
+import { PassThrough } from 'node:stream';
 
-import type { AppLoadContext, EntryContext } from "@remix-run/node";
-import { createReadableStreamFromReadable } from "@remix-run/node";
-import { RemixServer } from "@remix-run/react";
-import isbot from "isbot";
-import { renderToPipeableStream } from "react-dom/server";
+import type { AppLoadContext, EntryContext } from '@remix-run/node';
+import { createReadableStreamFromReadable } from '@remix-run/node';
+import { RemixServer } from '@remix-run/react';
+import isbot from 'isbot';
+import { renderToPipeableStream } from 'react-dom/server';
 
 const ABORT_DELAY = 5_000;
 
@@ -19,21 +19,11 @@ export default function handleRequest(
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext,
-  loadContext: AppLoadContext
+  _loadContext: AppLoadContext
 ) {
-  return isbot(request.headers.get("user-agent"))
-    ? handleBotRequest(
-        request,
-        responseStatusCode,
-        responseHeaders,
-        remixContext
-      )
-    : handleBrowserRequest(
-        request,
-        responseStatusCode,
-        responseHeaders,
-        remixContext
-      );
+  return isbot(request.headers.get('user-agent'))
+    ? handleBotRequest(request, responseStatusCode, responseHeaders, remixContext)
+    : handleBrowserRequest(request, responseStatusCode, responseHeaders, remixContext);
 }
 
 function handleBotRequest(
@@ -45,18 +35,14 @@ function handleBotRequest(
   return new Promise((resolve, reject) => {
     let shellRendered = false;
     const { pipe, abort } = renderToPipeableStream(
-      <RemixServer
-        context={remixContext}
-        url={request.url}
-        abortDelay={ABORT_DELAY}
-      />,
+      <RemixServer context={remixContext} url={request.url} abortDelay={ABORT_DELAY} />,
       {
         onAllReady() {
           shellRendered = true;
           const body = new PassThrough();
           const stream = createReadableStreamFromReadable(body);
 
-          responseHeaders.set("Content-Type", "text/html");
+          responseHeaders.set('Content-Type', 'text/html');
 
           resolve(
             new Response(stream, {
@@ -95,18 +81,14 @@ function handleBrowserRequest(
   return new Promise((resolve, reject) => {
     let shellRendered = false;
     const { pipe, abort } = renderToPipeableStream(
-      <RemixServer
-        context={remixContext}
-        url={request.url}
-        abortDelay={ABORT_DELAY}
-      />,
+      <RemixServer context={remixContext} url={request.url} abortDelay={ABORT_DELAY} />,
       {
         onShellReady() {
           shellRendered = true;
           const body = new PassThrough();
           const stream = createReadableStreamFromReadable(body);
 
-          responseHeaders.set("Content-Type", "text/html");
+          responseHeaders.set('Content-Type', 'text/html');
 
           resolve(
             new Response(stream, {
--- app/routes/projects.tsx
+++ app/routes/projects.tsx
@@ -1,4 +1,4 @@
-import { useLoaderData } from "@remix-run/react";
+import { useLoaderData } from '@remix-run/react';
 
 type Projects = {
   name: string;
@@ -12,24 +12,23 @@ type Projects = {
 export const loader = async () => {
   const projects: Projects[] = [
     {
-      name: "Chat App - Web",
-      logo: "https://chat-withme.vercel.app/icons/apple-touch-icon.png",
-      imageUrl:
-        "https://cdn.jsdelivr.net/gh/jatindotdev/chat-app/public/cover.png",
-      projectUrl: "https://chat-withme.vercel.app",
-      sourceUrl: "https://github.com/codewjatin/chat-app",
+      name: 'Chat App - Web',
+      logo: 'https://chat-withme.vercel.app/icons/apple-touch-icon.png',
+      imageUrl: 'https://cdn.jsdelivr.net/gh/jatindotdev/chat-app/public/cover.png',
+      projectUrl: 'https://chat-withme.vercel.app',
+      sourceUrl: 'https://github.com/codewjatin/chat-app',
       description:
-        "A sleek and minimalistic chat app built with HTML, CSS, and JavaScript, powered by Firebase for seamless communication and a user-friendly interface.",
+        'A sleek and minimalistic chat app built with HTML, CSS, and JavaScript, powered by Firebase for seamless communication and a user-friendly interface.',
     },
     {
-      name: "Music Player - Deno",
-      logo: "https://deno.land/logo.svg",
+      name: 'Music Player - Deno',
+      logo: 'https://deno.land/logo.svg',
       imageUrl:
-        "https://cdn.jsdelivr.net/gh/jatindotdev/deno-music-player/assets/cover.png",
-      projectUrl: "https://music-player.deno.dev",
-      sourceUrl: "https://github.com/codewjatin/deno-music-player",
+        'https://cdn.jsdelivr.net/gh/jatindotdev/deno-music-player/assets/cover.png',
+      projectUrl: 'https://music-player.deno.dev',
+      sourceUrl: 'https://github.com/codewjatin/deno-music-player',
       description:
-        "A Deno-powered music player with server-side query search and easy frontend integration for seamless listening experience.",
+        'A Deno-powered music player with server-side query search and easy frontend integration for seamless listening experience.',
     },
   ];
 
@@ -43,27 +42,28 @@ export default function Projects() {
       <div className="container mx-auto content">
         <h1 className="page-title">Projects</h1>
         <div>
-          -- Follow me on GitHub :){" "}
+          -- Follow me on GitHub :){' '}
           <a
-            href={"https://github.com/jatindotdev?repositories"}
+            href={'https://github.com/jatindotdev?repositories'}
             className="btn project-btn"
             target="_blank"
-            rel="noopener noreferrer">
+            rel="noopener noreferrer"
+          >
             github.com/jatindotdev
           </a>
         </div>
         <br />
         <div className="page-sub-heading">
-          I have developed a variety of projects as part of my learning process,
-          all open-sourced on my GitHub profile. I have shared some of these
-          projects for others to view. I am always looking for ways to improve
-          and expand my skills as a developer. b
+          I have developed a variety of projects as part of my learning process, all
+          open-sourced on my GitHub profile. I have shared some of these projects for
+          others to view. I am always looking for ways to improve and expand my skills as
+          a developer. b
         </div>
         <hr className="mt-xl mb-xxl" />
       </div>
       <div className="container-slim mx-auto content">
-        {projects.map((project) => {
-          const description = project.description.split("\n\n");
+        {projects.map(project => {
+          const description = project.description.split('\n\n');
           const projectUrl = new URL(project.projectUrl).hostname;
           return (
             <div key={project.name} className="project-container mb-xxl">
@@ -77,14 +77,15 @@ export default function Projects() {
                 />
               </div>
               <h4 className="project-title">{project.name}</h4>
-              {description.map((paragraph) => (
+              {description.map(paragraph => (
                 <p key={paragraph}>{paragraph}</p>
               ))}
               <a
                 href={project.projectUrl}
                 className="btn project-btn"
                 target="_blank"
-                rel="noopener noreferrer">
+                rel="noopener noreferrer"
+              >
                 {projectUrl}
                 <i className="icon icon-external-link"></i>
               </a>
@@ -92,7 +93,8 @@ export default function Projects() {
                 href={project.sourceUrl}
                 className="btn project-btn"
                 target="_blank"
-                rel="noopener noreferrer">
+                rel="noopener noreferrer"
+              >
                 {new URL(project.sourceUrl).pathname.slice(1)}
                 <i className="icon icon-external-link"></i>
               </a>
@@ -107,14 +109,14 @@ export default function Projects() {
         <div className="heading-group mb-m">
           <h4 className="mb-s">Acknowledgments</h4>
           <div className="sub-heading mb-s">
-            I'd like to extend my gratitude to all the developers who have put
-            in hard work to create resources for others to learn from and build
-            stunning websites, as well as to contribute to open-source projects.
+            I'd like to extend my gratitude to all the developers who have put in hard
+            work to create resources for others to learn from and build stunning websites,
+            as well as to contribute to open-source projects.
           </div>
           <p>
-            The sharing of knowledge and collaboration in the open-source
-            community is a fantastic way to advance technology. Keep up the good
-            work and continue making contributions!
+            The sharing of knowledge and collaboration in the open-source community is a
+            fantastic way to advance technology. Keep up the good work and continue making
+            contributions!
           </p>
         </div>
       </div>
