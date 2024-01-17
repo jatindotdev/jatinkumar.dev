09fa5f69f71c7f9ac0420531101bfa43a34e7429
Jatin Kumar <main.jatink@gmail.com>
2024-01-17 23:25:19 +0530

    chore: format

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,8 +1,8 @@
 export default function About() {
-  return (
-    <div className="main-wrapper">
-      <div className="container-slim mx-auto content">
-        {/* <div className="inset-image about-cover-image mb-m">
+	return (
+		<div className="main-wrapper">
+			<div className="container-slim mx-auto content">
+				{/* <div className="inset-image about-cover-image mb-m">
           <img
             height="520"
             width="880"
@@ -10,289 +10,318 @@ export default function About() {
             alt="Jatin"
           />
         </div> */}
-        <p>
-          I'm Jatin and I am a student pursuing a Bachelor's degree in Computer
-          Science. My specialty is in creating mobile and web-based applications
-          with a strong emphasis on simplicity and usability, with a focus on
-          building interfaces for the same.
-        </p>
-        <p>
-          You can also find me working on my own projects: such as a{" "}
-          <a
-            href="https://github.com/codewjatin/chat-app"
-            target="_blank"
-            rel="noopener noreferrer">
-            Chat App
-          </a>{" "}
-          built with HTML, CSS, and JavaScript, and a{" "}
-          <a
-            href="https://github.com/codewjatin/chat-app"
-            target="_blank"
-            rel="noopener noreferrer">
-            Music Player
-          </a>{" "}
-          using Deno with server-side search and easy frontend integration for a
-          smooth listening experience.
-        </p>
-        <p>
-          In my free time, I enjoy playing video games, watching sci-fi shows
-          and movies, experimenting with technology and taking pictures.
-        </p>
-        <p>
-          You can follow along with my work, personal projects and occasional
-          insights into my life on{" "}
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://twitter.com/p__jatin">
-            Twitter
-          </a>{" "}
-          and{" "}
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://instagram.com/p__jatin">
-            Instagram
-          </a>
-          .
-        </p>
-        <hr className="mt-xl mb-m" />
-        <h4>Development and Cloud</h4>
-        <p className="t-stack mb-m">
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://nodejs.org/en/">
-            NodeJS
-          </a>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.mongodb.com/">
-            MongoDB
-          </a>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.postgresql.org/">
-            PostgreSQL
-          </a>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.mysql.com/">
-            MySQL
-          </a>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://expressjs.com/">
-            ExpressJS
-          </a>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://serverless-stack.com/">
-            serverless stack
-          </a>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://reactjs.org/">
-            ReactJS
-          </a>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://remix.run/">
-            RemixJS
-          </a>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://nextjs.org/">
-            NextJS
-          </a>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://tailwindcss.com/">
-            Tailwind CSS
-          </a>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://chakra-ui.com/">
-            ChakraUI
-          </a>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://cloud.google.com/">
-            GCP
-          </a>
-          &nbsp;and more.
-        </p>
-        <h4>Programming Languages</h4>
-        <p className="t-stack">
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.typescriptlang.org/">
-            TypeScript
-          </a>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.javascript.com/">
-            JavaScript
-          </a>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.python.org/">
-            Python
-          </a>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.cplusplus.com/">
-            C++
-          </a>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://golang.org/">
-            GO
-          </a>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.oracle.com/java/">
-            Java
-          </a>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.microsoft.com/en-us/sql-server/sql-server-downloads">
-            SQL
-          </a>
-          &nbsp;and more.
-        </p>
-        <hr className="mt-xl mb-m" />
-        <h4>Education</h4>
-        <div className="work-list">
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            className="work-list-item"
-            href="https://climeclabs.com">
-            <span className="work-list-item-company">SRM University</span>
-            <span className="work-list-item-spacer" />
-            <span className="work-list-item-title">Full-Time Student</span>
-            <span className="work-list-item-date">CURRENT</span>
-          </a>
-          <ul>
-            <li>B.Tech in CSE</li>
-            <li>Overall CGPA: 8.07</li>
-          </ul>
-        </div>
-        <hr className="mt-m mb-xl" />
-        <h4>Professional Experience</h4>
-        <div className="work-list">
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            className="work-list-item"
-            href="https://srmist.edu.in">
-            <span className="work-list-item-company">Climec Labs</span>
-            <span className="work-list-item-spacer" />
-            <span className="work-list-item-title">SDE Intern</span>
-            <span className="work-list-item-date">CURRENT</span>
-          </a>
-          <ul>
-            <li>
-              Developed a Flutter application that seamlessly integrates with
-              WiFi-enabled Air Purifiers.
-            </li>
-            <li>
-              Built a Flutter app with Dart, seamlessly integrating Firebase as
-              the backend.
-            </li>
-            <li>
-              My contributions primarily centred on implementing device
-              connection functionality and designing the application's user
-              flow.
-            </li>
-          </ul>
-        </div>
-        <hr className="mt-m mb-xl" />
-        <h4>Organizations</h4>
-        <div className="work-list">
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            className="work-list-item"
-            href="https://www.srmkzilla.net/">
-            <span className="work-list-item-company">
-              SRMKZILLA (Official mozilla campus club)
-            </span>
-            <span className="work-list-item-spacer" />
-            <span className="work-list-item-title">Associate Tech Lead</span>
-            <span className="work-list-item-date">CURRENT</span>
-          </a>
-          <ul>
-            <li>
-              Started as a member, went on to become a associate technical lead
-              in my second year.
-            </li>
-            <li>
-              Worked on many open and closed source projects in MERN stack and
-              Flutter.
-            </li>
-            <li>
-              Created a Flutter application designed for managing event
-              check-ins and attendance.
-            </li>
-            <li>
-              Worked on the development of backend of a CMS in Node.js,
-              TypeScript.
-            </li>
-          </ul>
-        </div>
-        <hr className="mt-m mb-xl" />
-        <h4>About this site</h4>
-        <p>In case you were wondering this site is:</p>
-        <ul>
-          <li>
-            Designed and built on a Mac using{" "}
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://code.visualstudio.com/">
-              Visual Studio Code
-            </a>
-          </li>
-          <li>
-            Hosted and Deployed by{" "}
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://vercel.com">
-              Vercel
-            </a>
-          </li>
-          <li>
-            Inspired by{" "}
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://daleanthony.com">
-              daleanthony.com
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
+				<p>
+					I'm Jatin and I am a student pursuing a Bachelor's degree in Computer
+					Science. My specialty is in creating mobile and web-based applications
+					with a strong emphasis on simplicity and usability, with a focus on
+					building interfaces for the same.
+				</p>
+				<p>
+					You can also find me working on my own projects: such as a{" "}
+					<a
+						href="https://github.com/codewjatin/chat-app"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Chat App
+					</a>{" "}
+					built with HTML, CSS, and JavaScript, and a{" "}
+					<a
+						href="https://github.com/codewjatin/chat-app"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Music Player
+					</a>{" "}
+					using Deno with server-side search and easy frontend integration for a
+					smooth listening experience.
+				</p>
+				<p>
+					In my free time, I enjoy playing video games, watching sci-fi shows
+					and movies, experimenting with technology and taking pictures.
+				</p>
+				<p>
+					You can follow along with my work, personal projects and occasional
+					insights into my life on{" "}
+					<a
+						target="_blank"
+						rel="noopener noreferrer"
+						href="https://twitter.com/p__jatin"
+					>
+						Twitter
+					</a>{" "}
+					and{" "}
+					<a
+						target="_blank"
+						rel="noopener noreferrer"
+						href="https://instagram.com/p__jatin"
+					>
+						Instagram
+					</a>
+					.
+				</p>
+				<hr className="mt-xl mb-m" />
+				<h4>Development and Cloud</h4>
+				<p className="t-stack mb-m">
+					<a
+						target="_blank"
+						rel="noopener noreferrer"
+						href="https://nodejs.org/en/"
+					>
+						NodeJS
+					</a>
+					<a
+						target="_blank"
+						rel="noopener noreferrer"
+						href="https://www.mongodb.com/"
+					>
+						MongoDB
+					</a>
+					<a
+						target="_blank"
+						rel="noopener noreferrer"
+						href="https://www.postgresql.org/"
+					>
+						PostgreSQL
+					</a>
+					<a
+						target="_blank"
+						rel="noopener noreferrer"
+						href="https://www.mysql.com/"
+					>
+						MySQL
+					</a>
+					<a
+						target="_blank"
+						rel="noopener noreferrer"
+						href="https://expressjs.com/"
+					>
+						ExpressJS
+					</a>
+					<a
+						target="_blank"
+						rel="noopener noreferrer"
+						href="https://serverless-stack.com/"
+					>
+						serverless stack
+					</a>
+					<a
+						target="_blank"
+						rel="noopener noreferrer"
+						href="https://reactjs.org/"
+					>
+						ReactJS
+					</a>
+					<a
+						target="_blank"
+						rel="noopener noreferrer"
+						href="https://remix.run/"
+					>
+						RemixJS
+					</a>
+					<a
+						target="_blank"
+						rel="noopener noreferrer"
+						href="https://nextjs.org/"
+					>
+						NextJS
+					</a>
+					<a
+						target="_blank"
+						rel="noopener noreferrer"
+						href="https://tailwindcss.com/"
+					>
+						Tailwind CSS
+					</a>
+					<a
+						target="_blank"
+						rel="noopener noreferrer"
+						href="https://chakra-ui.com/"
+					>
+						ChakraUI
+					</a>
+					<a
+						target="_blank"
+						rel="noopener noreferrer"
+						href="https://cloud.google.com/"
+					>
+						GCP
+					</a>
+					&nbsp;and more.
+				</p>
+				<h4>Programming Languages</h4>
+				<p className="t-stack">
+					<a
+						target="_blank"
+						rel="noopener noreferrer"
+						href="https://www.typescriptlang.org/"
+					>
+						TypeScript
+					</a>
+					<a
+						target="_blank"
+						rel="noopener noreferrer"
+						href="https://www.javascript.com/"
+					>
+						JavaScript
+					</a>
+					<a
+						target="_blank"
+						rel="noopener noreferrer"
+						href="https://www.python.org/"
+					>
+						Python
+					</a>
+					<a
+						target="_blank"
+						rel="noopener noreferrer"
+						href="https://www.cplusplus.com/"
+					>
+						C++
+					</a>
+					<a
+						target="_blank"
+						rel="noopener noreferrer"
+						href="https://golang.org/"
+					>
+						GO
+					</a>
+					<a
+						target="_blank"
+						rel="noopener noreferrer"
+						href="https://www.oracle.com/java/"
+					>
+						Java
+					</a>
+					<a
+						target="_blank"
+						rel="noopener noreferrer"
+						href="https://www.microsoft.com/en-us/sql-server/sql-server-downloads"
+					>
+						SQL
+					</a>
+					&nbsp;and more.
+				</p>
+				<hr className="mt-xl mb-m" />
+				<h4>Education</h4>
+				<div className="work-list">
+					<a
+						target="_blank"
+						rel="noopener noreferrer"
+						className="work-list-item"
+						href="https://climeclabs.com"
+					>
+						<span className="work-list-item-company">SRM University</span>
+						<span className="work-list-item-spacer" />
+						<span className="work-list-item-title">Full-Time Student</span>
+						<span className="work-list-item-date">CURRENT</span>
+					</a>
+					<ul>
+						<li>B.Tech in CSE</li>
+						<li>Overall CGPA: 8.07</li>
+					</ul>
+				</div>
+				<hr className="mt-m mb-xl" />
+				<h4>Professional Experience</h4>
+				<div className="work-list">
+					<a
+						target="_blank"
+						rel="noopener noreferrer"
+						className="work-list-item"
+						href="https://srmist.edu.in"
+					>
+						<span className="work-list-item-company">Climec Labs</span>
+						<span className="work-list-item-spacer" />
+						<span className="work-list-item-title">SDE Intern</span>
+						<span className="work-list-item-date">CURRENT</span>
+					</a>
+					<ul>
+						<li>
+							Developed a Flutter application that seamlessly integrates with
+							WiFi-enabled Air Purifiers.
+						</li>
+						<li>
+							Built a Flutter app with Dart, seamlessly integrating Firebase as
+							the backend.
+						</li>
+						<li>
+							My contributions primarily centred on implementing device
+							connection functionality and designing the application's user
+							flow.
+						</li>
+					</ul>
+				</div>
+				<hr className="mt-m mb-xl" />
+				<h4>Organizations</h4>
+				<div className="work-list">
+					<a
+						target="_blank"
+						rel="noopener noreferrer"
+						className="work-list-item"
+						href="https://www.srmkzilla.net/"
+					>
+						<span className="work-list-item-company">
+							SRMKZILLA (Official mozilla campus club)
+						</span>
+						<span className="work-list-item-spacer" />
+						<span className="work-list-item-title">Associate Tech Lead</span>
+						<span className="work-list-item-date">CURRENT</span>
+					</a>
+					<ul>
+						<li>
+							Started as a member, went on to become a associate technical lead
+							in my second year.
+						</li>
+						<li>
+							Worked on many open and closed source projects in MERN stack and
+							Flutter.
+						</li>
+						<li>
+							Created a Flutter application designed for managing event
+							check-ins and attendance.
+						</li>
+						<li>
+							Worked on the development of backend of a CMS in Node.js,
+							TypeScript.
+						</li>
+					</ul>
+				</div>
+				<hr className="mt-m mb-xl" />
+				<h4>About this site</h4>
+				<p>In case you were wondering this site is:</p>
+				<ul>
+					<li>
+						Designed and built on a Mac using{" "}
+						<a
+							target="_blank"
+							rel="noopener noreferrer"
+							href="https://code.visualstudio.com/"
+						>
+							Visual Studio Code
+						</a>
+					</li>
+					<li>
+						Hosted and Deployed by{" "}
+						<a
+							target="_blank"
+							rel="noopener noreferrer"
+							href="https://vercel.com"
+						>
+							Vercel
+						</a>
+					</li>
+					<li>
+						Inspired by{" "}
+						<a
+							target="_blank"
+							rel="noopener noreferrer"
+							href="https://daleanthony.com"
+						>
+							daleanthony.com
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
 }
