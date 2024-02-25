ffd32bac6305ca0e39ea198aedf06e17762676a5
Jatin <main.jatink@gmail.com>
2024-02-25 14:16:56 +0530

    feat: use biome instead of eslint (#14)

--- app/root.tsx
+++ app/root.tsx
@@ -1,112 +1,112 @@
 import {
-	Links,
-	Meta,
-	Outlet,
-	Scripts,
-	ScrollRestoration,
-	type MetaFunction,
-} from "@remix-run/react";
-import "~/tailwind.css";
-import { Nav } from "./components/nav";
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  type MetaFunction,
+} from '@remix-run/react';
+import '~/tailwind.css';
+import { Nav } from './components/nav';
 
 export const meta: MetaFunction = () => {
-	const meta = {
-		title: "Jatin - Software Developer",
-		description:
-			"Software Developer learning and building applications, softwares and interfaces.",
-		image: "/assets/img/cover.png",
-		url: "https://jatinkumar.dev",
-		type: "website",
-	};
-	return [
-		{ title: meta.title },
-		{
-			tagName: "link",
-			rel: "icon",
-			href: "/favicon.ico",
-		},
-		{
-			tagName: "link",
-			rel: "icon",
-			href: "/logo.png",
-			type: "image/png",
-		},
-		{
-			name: "description",
-			content: meta.description,
-		},
-		{
-			tagName: "meta",
-			name: "og:title",
-			content: meta.title,
-		},
-		{
-			tagName: "meta",
-			name: "og:description",
-			content: meta.description,
-		},
-		{
-			tagName: "meta",
-			name: "og:image",
-			content: meta.image,
-		},
-		{
-			tagName: "meta",
-			name: "og:url",
-			content: meta.url,
-		},
-		{
-			tagName: "meta",
-			name: "og:type",
-			content: meta.type,
-		},
-		{
-			tagName: "meta",
-			name: "twitter:title",
-			content: meta.title,
-		},
-		{
-			tagName: "meta",
-			name: "twitter:description",
-			content: meta.description,
-		},
-		{
-			tagName: "meta",
-			name: "twitter:image",
-			content: meta.image,
-		},
-		{
-			tagName: "meta",
-			name: "twitter:url",
-			content: meta.url,
-		},
-		{
-			tagName: "meta",
-			name: "twitter:card",
-			content: "summary_large_image",
-		},
-	];
+  const meta = {
+    title: 'Jatin - Software Developer',
+    description:
+      'Software Developer learning and building applications, softwares and interfaces.',
+    image: '/assets/img/cover.png',
+    url: 'https://jatinkumar.dev',
+    type: 'website',
+  };
+  return [
+    { title: meta.title },
+    {
+      tagName: 'link',
+      rel: 'icon',
+      href: '/favicon.ico',
+    },
+    {
+      tagName: 'link',
+      rel: 'icon',
+      href: '/logo.png',
+      type: 'image/png',
+    },
+    {
+      name: 'description',
+      content: meta.description,
+    },
+    {
+      tagName: 'meta',
+      name: 'og:title',
+      content: meta.title,
+    },
+    {
+      tagName: 'meta',
+      name: 'og:description',
+      content: meta.description,
+    },
+    {
+      tagName: 'meta',
+      name: 'og:image',
+      content: meta.image,
+    },
+    {
+      tagName: 'meta',
+      name: 'og:url',
+      content: meta.url,
+    },
+    {
+      tagName: 'meta',
+      name: 'og:type',
+      content: meta.type,
+    },
+    {
+      tagName: 'meta',
+      name: 'twitter:title',
+      content: meta.title,
+    },
+    {
+      tagName: 'meta',
+      name: 'twitter:description',
+      content: meta.description,
+    },
+    {
+      tagName: 'meta',
+      name: 'twitter:image',
+      content: meta.image,
+    },
+    {
+      tagName: 'meta',
+      name: 'twitter:url',
+      content: meta.url,
+    },
+    {
+      tagName: 'meta',
+      name: 'twitter:card',
+      content: 'summary_large_image',
+    },
+  ];
 };
 
 export function Layout({ children }: { children: React.ReactNode }) {
-	return (
-		<html lang="en">
-			<head>
-				<meta charSet="utf-8" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<Meta />
-				<Links />
-			</head>
-			<body>
-				<Nav />
-				{children}
-				<ScrollRestoration />
-				<Scripts />
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Nav />
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
 }
 
 export default function App() {
-	return <Outlet />;
+  return <Outlet />;
 }
--- app/routes/projects.tsx
+++ app/routes/projects.tsx
@@ -1,126 +1,126 @@
-import { useLoaderData } from "@remix-run/react";
+import { useLoaderData } from '@remix-run/react';
 
 type Project = {
-	name: string;
-	logo: string;
-	imageUrl: string;
-	projectUrl: string;
-	sourceUrl: string;
-	description: string;
+  name: string;
+  logo: string;
+  imageUrl: string;
+  projectUrl: string;
+  sourceUrl: string;
+  description: string;
 };
 
 export const loader = async () => {
-	const projects: Project[] = [
-		{
-			name: "Chat App - Web",
-			logo: "https://chat-withme.vercel.app/icons/apple-touch-icon.png",
-			imageUrl:
-				"https://cdn.jsdelivr.net/gh/jatindotdev/chat-app/public/cover.png",
-			projectUrl: "https://chat-withme.vercel.app",
-			sourceUrl: "https://github.com/codewjatin/chat-app",
-			description:
-				"A sleek and minimalistic chat app built with HTML, CSS, and JavaScript, powered by Firebase for seamless communication and a user-friendly interface.",
-		},
-		{
-			name: "Music Player - Deno",
-			logo: "https://upload.wikimedia.org/wikipedia/commons/8/84/Deno.svg",
-			imageUrl:
-				"https://cdn.jsdelivr.net/gh/codewjatin/deno-music-player/assets/cover.png",
-			projectUrl: "https://music-player.deno.dev",
-			sourceUrl: "https://github.com/codewjatin/deno-music-player",
-			description:
-				"A Deno-powered music player with server-side query search and easy frontend integration for seamless listening experience.",
-		},
-	];
+  const projects: Project[] = [
+    {
+      name: 'Chat App - Web',
+      logo: 'https://chat-withme.vercel.app/icons/apple-touch-icon.png',
+      imageUrl:
+        'https://cdn.jsdelivr.net/gh/jatindotdev/chat-app/public/cover.png',
+      projectUrl: 'https://chat-withme.vercel.app',
+      sourceUrl: 'https://github.com/codewjatin/chat-app',
+      description:
+        'A sleek and minimalistic chat app built with HTML, CSS, and JavaScript, powered by Firebase for seamless communication and a user-friendly interface.',
+    },
+    {
+      name: 'Music Player - Deno',
+      logo: 'https://upload.wikimedia.org/wikipedia/commons/8/84/Deno.svg',
+      imageUrl:
+        'https://cdn.jsdelivr.net/gh/codewjatin/deno-music-player/assets/cover.png',
+      projectUrl: 'https://music-player.deno.dev',
+      sourceUrl: 'https://github.com/codewjatin/deno-music-player',
+      description:
+        'A Deno-powered music player with server-side query search and easy frontend integration for seamless listening experience.',
+    },
+  ];
 
-	return { projects };
+  return { projects };
 };
 
 export default function Projects() {
-	const { projects } = useLoaderData<typeof loader>();
-	return (
-		<div className="main-wrapper">
-			<div className="container mx-auto content">
-				<h1 className="page-title">Projects</h1>
-				<div>
-					-- Follow me on GitHub :){" "}
-					<a
-						href={"https://github.com/jatindotdev?repositories"}
-						className="btn project-btn"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						github.com/jatindotdev
-					</a>
-				</div>
-				<br />
-				<div className="page-sub-heading">
-					I have developed a variety of projects as part of my learning process,
-					all open-sourced on my GitHub profile. I have shared some of these
-					projects for others to view. I am always looking for ways to improve
-					and expand my skills as a developer. b
-				</div>
-				<hr className="mt-xl mb-xxl" />
-			</div>
-			<div className="container-slim mx-auto content">
-				{projects.map((project) => {
-					const description = project.description.split("\n\n");
-					const projectUrl = new URL(project.projectUrl).hostname;
-					return (
-						<div key={project.name} className="project-container mb-xxl">
-							<div className="inset-image project-image mb-m">
-								<img
-									alt={project.name}
-									loading="lazy"
-									width={880}
-									height={495}
-									src={project.imageUrl}
-								/>
-							</div>
-							<h4 className="project-title">{project.name}</h4>
-							{description.map((paragraph) => (
-								<p key={paragraph}>{paragraph}</p>
-							))}
-							<a
-								href={project.projectUrl}
-								className="btn project-btn"
-								target="_blank"
-								rel="noopener noreferrer"
-							>
-								{projectUrl}
-								<i className="icon icon-external-link" />
-							</a>
-							<a
-								href={project.sourceUrl}
-								className="btn project-btn"
-								target="_blank"
-								rel="noopener noreferrer"
-							>
-								{new URL(project.sourceUrl).pathname.slice(1)}
-								<i className="icon icon-external-link" />
-							</a>
-						</div>
-					);
-				})}
-			</div>
-			<div className="container mx-auto">
-				<hr className="mt-xxl mb-xxl" />
-			</div>
-			<div className="container-slim mx-auto">
-				<div className="heading-group mb-m">
-					<h4 className="mb-s">Acknowledgments</h4>
-					<div className="sub-heading mb-s">
-						I'd like to extend my gratitude to all the developers who have put
-						in hard work to create resources for others to learn from and build
-						stunning websites, as well as to contribute to open-source projects.
-					</div>
-					<p>
-						The sharing of knowledge and collaboration in the open-source
-						community is a fantastic way to advance technology. Keep up the good
-						work and continue making contributions!
-					</p>
-				</div>
-			</div>
-		</div>
-	);
+  const { projects } = useLoaderData<typeof loader>();
+  return (
+    <div className="main-wrapper">
+      <div className="container mx-auto content">
+        <h1 className="page-title">Projects</h1>
+        <div>
+          -- Follow me on GitHub :){' '}
+          <a
+            href={'https://github.com/jatindotdev?repositories'}
+            className="btn project-btn"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            github.com/jatindotdev
+          </a>
+        </div>
+        <br />
+        <div className="page-sub-heading">
+          I have developed a variety of projects as part of my learning process,
+          all open-sourced on my GitHub profile. I have shared some of these
+          projects for others to view. I am always looking for ways to improve
+          and expand my skills as a developer. b
+        </div>
+        <hr className="mt-xl mb-xxl" />
+      </div>
+      <div className="container-slim mx-auto content">
+        {projects.map(project => {
+          const description = project.description.split('\n\n');
+          const projectUrl = new URL(project.projectUrl).hostname;
+          return (
+            <div key={project.name} className="project-container mb-xxl">
+              <div className="inset-image project-image mb-m">
+                <img
+                  alt={project.name}
+                  loading="lazy"
+                  width={880}
+                  height={495}
+                  src={project.imageUrl}
+                />
+              </div>
+              <h4 className="project-title">{project.name}</h4>
+              {description.map(paragraph => (
+                <p key={paragraph}>{paragraph}</p>
+              ))}
+              <a
+                href={project.projectUrl}
+                className="btn project-btn"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {projectUrl}
+                <i className="icon icon-external-link" />
+              </a>
+              <a
+                href={project.sourceUrl}
+                className="btn project-btn"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {new URL(project.sourceUrl).pathname.slice(1)}
+                <i className="icon icon-external-link" />
+              </a>
+            </div>
+          );
+        })}
+      </div>
+      <div className="container mx-auto">
+        <hr className="mt-xxl mb-xxl" />
+      </div>
+      <div className="container-slim mx-auto">
+        <div className="heading-group mb-m">
+          <h4 className="mb-s">Acknowledgments</h4>
+          <div className="sub-heading mb-s">
+            I'd like to extend my gratitude to all the developers who have put
+            in hard work to create resources for others to learn from and build
+            stunning websites, as well as to contribute to open-source projects.
+          </div>
+          <p>
+            The sharing of knowledge and collaboration in the open-source
+            community is a fantastic way to advance technology. Keep up the good
+            work and continue making contributions!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
